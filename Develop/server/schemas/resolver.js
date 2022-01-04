9b267f5227e0fc21d988890bc7dd1c31504baaf4
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2022-01-03 18:55:42 -0600

    authenticators added

---
+++ Develop/server/schemas/resolver.js
@@ -0,0 +1,45 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("books");
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      try {
+        const user = await User.create(args);
+
+        const token = signToken(user);
+        return { token, user };
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect email");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
